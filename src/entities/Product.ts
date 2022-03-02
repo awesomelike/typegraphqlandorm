860c302a439806e10f59df5667d72b94ec4f1df7
awesomelike <abdullaev.dev@gmail.com>
2022-03-03 02:17:46 +0500

    step-4

--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -1,3 +1,4 @@
+import { IsPositive, Length, MaxLength, MinLength } from 'class-validator';
 import { ObjectType, InputType, Field, ID, Float } from 'type-graphql';
 import { Category } from './Category';
 
@@ -22,12 +23,15 @@ export class Product {
 @InputType()
 export class CreateProductInput {
   @Field()
+  @Length(3, 255)
   title: string;
 
   @Field()
+  @MaxLength(255)
   description: string;
 
   @Field(() => Float)
+  @IsPositive()
   price: number;
 
   @Field()
